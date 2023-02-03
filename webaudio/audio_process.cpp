573200126a44813615767ecdfb1cff7a3b94470e
Iain Duncan <iduncan@xornot.com>
2023-02-03 09:53:24 -0800

    added simple scheduler

--- webaudio/audio_process.cpp
+++ webaudio/audio_process.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <cstdint>
+#include <math.h>
 #include <emscripten.h>
 
 #ifdef __cplusplus
@@ -12,7 +13,7 @@ const int AUDIO_BLOCK_SIZE = 128;
 const int SAMPLE_RATE = 44100;
 const int OSC_TABLE_SIZE = 1024;
 const int NUM_VOICES = 4;
-enum shape { SINE, SAW, SQUARE, NOISE};
+enum shape { SINE, SAW, TRIANGLE, SQUARE, PULSE};
 
 // a global static buffer for handling incoming event messages
 float evt_buf[EVT_BUF_SIZE];
@@ -30,7 +31,7 @@ float* getOutBuf() { return &g_out_buf[0]; }
 // shares one freq and phase, but can output three different waves
 // they just use dumb geometric tables for now (aliases badly)
 class WaveTableOsc {
-  static const int waveforms = 4;
+  static const int waveforms = 5;
   public:
     int sr;
     // a phasor value that runs from 0 to 1
@@ -40,14 +41,15 @@ class WaveTableOsc {
     // levels of sine, saw, square, noise
     float levels[waveforms];
 
-    void init(float start_freq, float sine_lvl, float saw_lvl, float square_lvl){
+    void init(float start_freq, float sine_lvl, float saw_lvl, float tri_lvl,
+     float square_lvl, float pulse_lvl){
         freq = start_freq;
         levels[SINE] = sine_lvl;
         levels[SAW] = saw_lvl;
+        levels[TRIANGLE] = tri_lvl;
         levels[SQUARE] = square_lvl;
-        initSine();
-        initSaw();
-        initSquare();
+        levels[PULSE] = pulse_lvl;
+        initWaveTables();
     }
 
     void reset(){ phasor = 0.0; }
@@ -55,23 +57,28 @@ class WaveTableOsc {
     // freq setter, stuff to implement glide will go here later
     void setFreq(float frq){ freq = frq; }
 
-    void initSine(){
-      // TODO formula for sine here yo
-    }
-    void initSaw(){
+    void initWaveTables(){
+      // TODO: SINE
+      // SAW
       for(int i=0; i < OSC_TABLE_SIZE; i++)
-        wavetables[SAW][i] = (1.0 / OSC_TABLE_SIZE) * i;
-    }
-    void initSquare(){
+        wavetables[SAW][i] = ((2.0 / OSC_TABLE_SIZE) * i) - 1.0;
+      // TRI
+      for(int i = 0; i < OSC_TABLE_SIZE / 2; i++)
+        wavetables[TRIANGLE][i] = ((2.0 / (OSC_TABLE_SIZE / 2)) * i) - 1.0;
+      for(int i = OSC_TABLE_SIZE / 2; i < OSC_TABLE_SIZE; i++)
+        wavetables[TRIANGLE][i] = wavetables[TRIANGLE][ i - (OSC_TABLE_SIZE / 2)] * -1.0;
+      // SQUARE
       for(int i=0; i < OSC_TABLE_SIZE; i++)
         wavetables[SQUARE][i] = i < OSC_TABLE_SIZE / 2 ? -1 : 1;
+      // PULSE, fixed 10% duty cycle
+      for(int i=0; i < OSC_TABLE_SIZE; i++)
+        wavetables[PULSE][i] = i < OSC_TABLE_SIZE / 10 ? -1 : 1;
     }
 
     float renderSample(){
       // update the internal phasor and use it to look up table
       phasor = phasor + (freq / SAMPLE_RATE);
-      if(phasor >= 1.0)
-        phasor -= 1.0;
+      if(phasor >= 1.0) phasor = 0;
       int tableIndex = (int) (OSC_TABLE_SIZE * phasor);
       float outSample = 0;
       for(int i=0; i < waveforms; i++){
@@ -151,28 +158,30 @@ class Voice {
     float levels[NUM_OSC];
     float _gain;
 
-    void init(float gain, float sineLevel, float sawLevel, float squareLevel){
+    void init(float gain, float sineLevel, float sawLevel, float triLevel, float squareLevel, float pulseLevel){
       for(int i=0; i < NUM_OSC; i++){
         // add some chorus be detuning the oscillators
         float frq = startFreq + (i * 0.001 * startFreq);
-        oscillators[i].init(frq, sineLevel, sawLevel, squareLevel);
+        oscillators[i].init(frq, sineLevel, sawLevel, triLevel, squareLevel, pulseLevel);
         // start with osc levels as 1.0 / NUM_OSC
         levels[i] = 1.0 / NUM_OSC;
       }
       // init the amp env with an attack and decay time
-      ampEnv.init(0.2, 0.5);
+      ampEnv.init(0.01, 0.1);
       _gain = gain;
     }
 
-    void setFreq(float freq){
+    void setFreq(float note){
       for(int i=0; i < NUM_OSC; i++){
+        // conver midi note to hz
+        float freq = (440.0 / 32) * pow(2, ((note - 9) / 12.0));
         oscillators[i].setFreq(freq);
       }
     }
 
-    void playNote(float dur, float amp, float frq){
+    void playNote(float dur, float amp, float noteNum){
       // set the osc frq and trigger the env
-      setFreq(frq);
+      setFreq(noteNum);
       ampEnv.trigger(dur);
     }
 
@@ -187,7 +196,7 @@ class Voice {
 };
 
 class Engine {
-  const static int NUM_VOICES = 1;
+  const static int NUM_VOICES = 4;
 
   public:
     int sampleRate;
@@ -198,17 +207,17 @@ class Engine {
 
     int init(int sampleRate, int blockSize, int numVoices){
       for(int i=0; i < NUM_VOICES; i++){
-        // initial voice levels for gain, sine, saw, square
-        voices[i].init(1.0, 0.0, 1.0, 1.0);
+        // initial voice levels for gain, sine, saw, tri, square, pulse
+        //voices[i].init(1.0, 0.0, 1.0, 0.0, 0.0, 0.0);
+        voices[i].init(1.0, 0.0, 0.0, 0.5, 0.2, 0.0); // SQR
       }
       perVoiceGain = 1.0 / NUM_VOICES;
       return 1;
     }
 
-    // for now, just delegate to the first voice
-    // smart voice allocation will go here later
-    void playNote(float dur, float amp, float freq){
-      voices[0].playNote(dur, amp, freq);
+    // for now, voice allocation is coming from outside the engine
+    void playNote(int voice, float dur, float amp, float noteNum){
+      voices[voice].playNote(dur, amp, noteNum);
     }
 
     // render an entire block of samples, writing to the global output buffer
@@ -246,6 +255,74 @@ float genNoise(){
 // we will use explicit init methods instead of constructors
 Engine engine;
 
+struct SchedulerEvent {
+  int active;
+  float time;
+  float params[4];
+};
+
+class Scheduler {
+  // Max number of events that can be on the scheduler
+  static const int SIZE = 1024;
+  // number of float data points per event
+  static const int PARAMS = 4;
+
+  private:
+    SchedulerEvent data[SIZE];
+    int numScheduled = 0;
+    float timeNow;
+
+  public:
+
+    // add an event to the scheduler
+    // looks for first empty row and uses it
+    int add(float time, float p1, float p2, float p3, float p4){
+      for(int i = 0; i < SIZE; i++){
+        if( data[i].active )
+          continue;
+        else {
+          data[i].active = 1;
+          data[i].time = timeNow + time;
+          data[i].params[0] = p1;
+          data[i].params[1] = p2;
+          data[i].params[2] = p3;
+          data[i].params[3] = p4;
+          numScheduled += 1;
+          break;
+        }
+      }
+      return 1;
+    }
+
+    void seek(float time){
+      timeNow = time;
+    }
+
+    void processBlock(){
+      // so we don't go through the whole data store unnecessarily
+      int eventsSeen = 0;
+      int eventsToSee = numScheduled;
+
+      for(int i = 0; i < SIZE; i++){
+        if( data[i].active && data[i].time > timeNow ){
+          eventsSeen += 1;
+        }
+        if( data[i].active  && data[i].time <= timeNow ){
+          engine.playNote( (int) data[i].params[0], data[i].params[1], data[i].params[2], data[i].params[3] );
+          data[i].active = 0;
+          numScheduled -= 1;
+          eventsSeen += 1;
+        }
+        // once we have seen all the events that were scheduled when we entered this function
+        // we can stop looking for events in the store
+        if( eventsSeen == eventsToSee)
+          break;
+      }
+      timeNow += (1.0 / SAMPLE_RATE) * AUDIO_BLOCK_SIZE;
+    }
+};
+Scheduler scheduler;
+
 // FFI function to setup our audio engine
 // this is called from JS
 int initEngine(){
@@ -255,27 +332,33 @@ int initEngine(){
 
 // FFI function called on incoming events once per block
 // JS side will fill the buffer and this will empty it
-// TODO: make this use a ring buffer and accept/send number of events
-int processEvents(){
-    int i = 0;
-    while( evt_buf[i] != 0){
-      float time = evt_buf[i + 1];
-      float dur  = evt_buf[i + 2];
-      float amp  = evt_buf[i + 3];
-      float freq = evt_buf[i + 4];
-      engine.playNote(dur, amp, freq);
-      // set flag bit to indicate evt processed
-      evt_buf[i] = 0;
-      i += 5;
+// FUTURE: make this use a ring buffer and accept/send number of events
+// each event is 8 floats in the buf
+int processEvents(int numEvents){
+    for(int i = 0; i < numEvents * 8; i += 8){
+      int voice   = (int) evt_buf[i];
+      float time  = evt_buf[i + 1];
+      float dur   = evt_buf[i + 2];
+      float amp   = evt_buf[i + 3];
+      float freq  = evt_buf[i + 4];
+      if(time <= 0){
+        engine.playNote(voice, dur, amp, freq);
+      }else{
+        scheduler.add(time, voice, dur, amp, freq);
+      }
     }
     return 1;
 }
+
 
 // FFI function that JS calls once per block to render a block of audio
 int processAudio(){
     if( engine.blockNumber == 0){
       // anything that should play automatically on start goes here
+      // seek scheduler to 0
+      scheduler.seek(0);
     }
+    scheduler.processBlock();
     engine.renderAudioBlock();
     return 1;
 }
--- webaudio/processor.js
+++ webaudio/processor.js
@@ -69,12 +69,17 @@ class AudioProcessor extends AudioWorkletProcessor {
     console.log("AudioProcessor.noteMsg() data:", noteData);
     // get at the evt buf
     var buf = new Float32Array(this.wa_buf, this.instance.exports.getEvtBuf(), EVT_BUF_SIZE);
-    // write an event, which consists of five floats
-    // flag, time, dur, amp, freq
-    buf.set( [1, 0.0, 2.0, 1.0, 110], 0);
-    // TODO use a proper ring buffer here
-    // call into C function to handle it
-    this.instance.exports.processEvents();
+    // write an event, which consists of five floats, but we will use 8 points per line
+    // voice, time, dur, amp, freq
+    buf.set( [1, 0.0, 1.0, 1.0, 60], 0);
+    buf.set( [2, 0.0, 1.0, 1.0, 64], 8);
+    buf.set( [3, 0.0, 1.0, 1.0, 67], 16);
+    // call C, tell it how many events there are
+    buf.set( [1, 2.0, 1.0, 1.0, 60], 24);
+    buf.set( [2, 2.0, 1.0, 1.0, 64], 32);
+    buf.set( [3, 2.0, 1.0, 1.0, 67], 40);
+    var res = this.instance.exports.processEvents(6);
+    console.log("got", res);
   }
 
   // log helper for in the audio loop, logs every X samples
@@ -106,7 +111,7 @@ class AudioProcessor extends AudioWorkletProcessor {
         // because we can't easily pass in a pointer to a block in a WASM worklet
         this.instance.exports.processAudio();
         for(var i=0; i < BLOCK_SIZE; i++){
-          output[channel][i] = this.audioOut[i] * gain * 0.1;
+          output[channel][i] = this.audioOut[i] * gain * 0.5;
         }
       }
     }
